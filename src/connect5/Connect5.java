88bd1e089a33bfa65329dda96cde53966839dd7a
Aluminium <Aluminium@192.168.0.3>
2014-02-08 14:41:58 -0700

    added classes/ints

--- src/connect5/Connect5.java
+++ src/connect5/Connect5.java
@@ -152,10 +152,10 @@ class HelpMenuView  {
         System.out.println();
         this.displayHelpBorder();             
         System.out.println( 
-                "\tThe game board for Tic-Tac-Toe. It consist of a grid of "
+                "\tThe game board for connect4. It consist of a grid of "
                 + "\n\tlocations. Players place their marker on the different locations "
                 + "\n\ton the board in an effort to win the game. The default board is "
-                + "\n\t3 rows by 3 columns.");
+                + "\n\t6 rows by 7 columns.");
         displayHelpBorder();
     }
     
@@ -165,10 +165,10 @@ class HelpMenuView  {
         System.out.println();
         displayHelpBorder();     
         System.out.println( 
-                 "\tThe objective of the game is to be the first player to mark three "
+                 "\tThe objective of the game is to be the first player to mark four "
                 + "\n\tsquares vertically, horizontally or diagonally. Each player takes "
                 + "\n\tturns placing their marker in one of the locations on the "
-                + "\n\tboard. The first player to get \"three-in-a-row\" is the winner."
+                + "\n\tboard. The first player to get \"four-in-a-row\" is the winner."
                 ); 
         displayHelpBorder();
     }
