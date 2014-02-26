811d5f43079c11db9fd454476353ad825921f6a6
Aluminium <Aluminium@192.168.0.3>
2014-02-25 20:00:54 -0700

    added classes/ints

--- src/connect5/MinMax.java
+++ src/connect5/MinMax.java
@@ -9,18 +9,22 @@ package connect5;
  * @author Aluminium
  */
 public class MinMax {
-    int nums[] = new int[5];
+    int num[] = new int[5];
     int min, max;
     MinmMaxView myMinmMaxView = new MinmMaxView();
     public MinMax()
     {
         
-//    would like to averege the numbers received from MinmMaxView.java
+
     num[0]=myMinmMaxView.g1;
     num[1]=myMinmMaxView.g2;
     num[2]=myMinmMaxView.g3;
     num[3]=myMinmMaxView.g4;
     num[4]=myMinmMaxView.g5;
+//    I would like for the loop to add each element in the array and give me the sum below
+    for(int i=0;i <num.length; i++)
+        num[i] = num[i]+num[i];
+//    Right here I need code by which to divide by num.length and therefore gain my average... I just don't know how to create
+//    the sum of my for loop
 }
 }
-//I would like to convert strings from MinmMaxView.java to integers for math
